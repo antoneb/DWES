8168be9630a5e8c11d469b74b6ad7610110b8aeb
antoneb <aespineirab24@fpcoruna.afundacion.org>
2024-10-16 12:54:17 +0200

    Subida ejercicios iniciales php

---
+++ sprint1apache/www/site3/example.php
@@ -0,0 +1,10 @@
+<html>
+
+<body>
+    <h1>Mi superheroe favorito...</h1>
+    <?php
+    $nombre = "Jorge el Fuerte";
+    echo $nombre;
+    ?>
+</body>
+</html>
---
+++ sprint1apache/www/site3/example2.php
@@ -0,0 +1,14 @@
+<html>
+
+<body>
+    <h1>Página de bienvenidas</h1>
+    <?php
+    function dar_bienvenida($nombre)
+    {
+        echo "¡Bienvenido/a, " . $nombre . "!";
+    }
+    dar_bienvenida("Guantes el gato");
+    ?>
+</body>
+
+</html>
