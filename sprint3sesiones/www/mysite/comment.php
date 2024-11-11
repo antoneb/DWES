8997d2ffb01f3d06c8b706c3a8825d0cb53bf56a
antoneb <aespineirab24@fpcoruna.afundacion.org>
2024-11-11 20:10:08 +0100

    Ejercicio4 comment con id

--- sprint3sesiones/www/mysite/comment.php
+++ sprint3sesiones/www/mysite/comment.php
@@ -1,15 +1,21 @@
 <?php
 $db = mysqli_connect('localhost', 'root', '1234', 'dbLibrosAnton') or die('Fail');
+session_start();
 ?>
 <html>
 
 <body>
     <?php
+
+    $user_id_a_insertar = 'NULL';
+    if (!empty($_SESSION['user_id'])) {
+        $user_id_a_insertar = $_SESSION['user_id'];
+    }
     $libro_id = $_POST['libro_id'];
     $comentario = $_POST['new_comment'];
     $fechaPost = time();
     $query = "INSERT INTO tComentarios(comentario, fechaPost, libro_id, usuario_id)
-VALUES ('" . $comentario . "',CURRENT_TIMESTAMP," . $libro_id . ",NULL)";
+VALUES ('" . $comentario . "',CURRENT_TIMESTAMP," . $libro_id . "," . $user_id_a_insertar . ")";
     mysqli_query($db, $query) or die('Error');
     echo "<p>Nuevo comentario ";
     echo mysqli_insert_id($db);
