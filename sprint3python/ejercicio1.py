c5915ee3f1695d0810fb65fa5ff27338ca824aa3
antoneb <aespineirab24@fpcoruna.afundacion.org>
2024-11-14 16:48:47 +0100

    Ejercicio 1 y 2 Python

---
+++ sprint3python/ejercicio1.py
@@ -0,0 +1,13 @@
+# if __name == '__main__' 
+# se utiliza para saber si un modulo de python fue ejecutado
+# directamente o si en vez de eso fue importado desde otro
+# modulo (archivo) y ejecutado desde este.
+# Es util para manejar distintos modulos de python y que no haya
+# discrepancias en el uso de variables o funciones, ya que
+# el propio interprete se encarga de diferenciar su origen
+# 
+# En mi caso lo usaré en el ejercicio 2, ya que debo importar
+# las funciones del 1, y en el ejercicio 3, para asegurar que el
+# juego se inicia desde el modulo adecuado
+# #
+
---
+++ sprint3python/operaciones.py
@@ -0,0 +1,15 @@
+    
+def suma (a,b):
+    return a+b 
+
+def resta(a,b):
+    return a-b
+
+def multi(a,b):
+    return a*b
+
+def divi(a,b):
+    if (b==0):
+        print("No se puede dividir entre 0")
+    else:
+        return a/b
